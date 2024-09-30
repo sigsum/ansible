19e7a6ca739601f5273d2539eeb31aef56021f7a
Linus Nordberg <linus.nordberg@glasklarteknik.se>
2024-09-30 08:10:21 +0200

    maria*snap.sh: Non-compatible changes to configuration!
    Read ~/.config/mariasnap/lv instead of /root/.config/mariasnap/vg,
    with a new format.
    
    Quote all environment variables.
    
    Document the configuration.

--- roles/mariadb/files/mariarmsnap.sh
+++ roles/mariadb/files/mariarmsnap.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
+set -eu
 
 # Run this script after the offline backup has been made, when
 # mariasnap.sh has been run before that.
 
-set -eu
-[ -s /root/.config/mariasnap/vg ]
+# Configuration and setup:
+# ~/.config/mariasnap/lv contains one line with the name of the
+#                        LV where the database lives, on the form VG/LV.
+# Example: echo vg-sigsum-log-02-db/db > /root/.config/mariasnap/lv
 
-VG="$(cat /root/.config/mariasnap/vg)"
+[ -s ~/.config/mariasnap/lv ]	# Ensure non-empty file.
+LV="$(cat ~/.config/mariasnap/lv)"
+VG="$(dirname "$LV")"
+
 SNAPNAME=dbsnap
 BUPDIR=/var/backups/dbsnap
 
-umount $BUPDIR || true          # Don't exit on failure
-lvremove -qq -y $VG/$SNAPNAME
+umount "$BUPDIR" || true	# Don't exit on failure
+lvremove -qq -y "$VG"/"$SNAPNAME"
--- roles/mariadb/files/mariasnap.sh
+++ roles/mariadb/files/mariasnap.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
+set -eu
 
 # Run this script before taking an offline backup and make sure that
 # the offline backup includes $BUPDIR (set below).
-
 # Run the accompanying mariarmsnap.sh script after taking the offline
 # backup.
 
-set -eu
-[ -s /root/.config/mariasnap/vg ]
+# Configuration and setup:
+# ~/.config/mariasnap/lv contains one line with the name of the
+#                        LV where the database lives, on the form VG/LV.
+# Example: echo vg-sigsum-log-02-db/db > /root/.config/mariasnap/lv
 
-VG="$(cat /root/.config/mariasnap/vg)"
-LV=$VG/db
+[ -s ~/.config/mariasnap/lv ]	# Ensure non-empty file.
+LV="$(cat ~/.config/mariasnap/lv)"
+VG="$(dirname "$LV")"
 
 SNAPNAME=dbsnap
 BUPDIR=/var/backups/dbsnap
@@ -26,8 +29,8 @@ BUPDIR=/var/backups/dbsnap
 
 env LVM_SUPPRESS_FD_WARNINGS=1 mariadb <<EOF
 flush tables with read lock;
-system lvcreate -qq -l 100%FREE --snapshot -n $SNAPNAME $LV;
+system lvcreate -qq -l 100%FREE --snapshot -n "$SNAPNAME" "$LV";
 EOF
 
-[ -d $BUPDIR ] || { mkdir -p $BUPDIR; chmod 700 $BUPDIR; }
-mount -o ro $VG/$SNAPNAME $BUPDIR
+[ -d "$BUPDIR" ] || { mkdir -p "$BUPDIR"; chmod 700 "$BUPDIR"; }
+mount -o ro /dev/"$VG"/"$SNAPNAME" "$BUPDIR"
